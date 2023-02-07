a431403b9d48ea2dd1520403e5cd2cf475300b09
JoseLeviRivera <joselevirivera@gmail.com>
2023-02-07 02:12:58 -0600

    Agregacion de la interfaces graficas, falta mas funcionalidades

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,10 @@ package org.example;
 import org.example.ScannerModels.ScannerAlumno;
 
 public class Main {
+
+    /*
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
     }
+     */
 }
