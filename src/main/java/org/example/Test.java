ccc2474299d2f8eac6fe5056db14890d4140bc8c
JoseLeviRivera <joselevirivera@gmail.com>
2023-02-08 16:58:02 -0600

    Commiteando .class

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.Respositorios.RepositorioAlumno;
+import org.example.SuperFuncion.SuperFuncion;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-
+        List<Object> lista = SuperFuncion.listar(new RepositorioAlumno());
+        lista.forEach(System.out::println);
     }
 }
